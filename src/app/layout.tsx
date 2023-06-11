8a3afde79f724cd6f27237931f592758eb235c40
RJ Villanueva <rj.villanueva89@gmail.com>
2023-06-11 15:27:38 +0800

    minor bg fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="h-screen w-full flex items-center justify-center">
+        <div className="h-screen w-full flex items-center justify-center bg-gray-100">
           <div className="w-full md:w-[40rem] p-4">{children}</div>
         </div>
       </body>
